d8b3ea0313d2fcc3edc49ccf7f8db5e68855652a
AJ Ancheta and Lindsey Baker <aj@>
2019-03-28 11:32:42 -0700

    Add fuction to remove and add text based on selector input

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,29 @@
 $(document).ready(function(){
   $("#selector").submit(function(event){
     var animalInput = $("#animal").val();
-    // var animalOutput = selectAnimal(animalInput)
+    var animalOutput = selectAnimal(animalInput)
+
+    event.preventDefault();
+  });
+
+});
+
 
   function selectAnimal(animalInput){
+    // debugger;
     if (animalInput === "turtles"){
-      $("#turtlesText").show();
+      $(".turtlesText").show();
+      $(".insectsText").hide()
+      $(".snakesText").hide()
     } else if (animalInput === "snakes"){
-      $("#snakesText").show();
+      $(".snakesText").show();
+      $(".insectsText").hide()
+      $(".turtlesText").hide()
     } else if (animalInput === "insects"){
-      $("#insectsText").show();
-});
-
-      event.preventDefault();
-});
+      $(".insectsText").show();
+      $(".turtlesText").hide()
+      $(".snakesText").hide()
+    }
+};
 
 //Back End
